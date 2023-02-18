0a02346b6e920158a789fef04d9cd8e04df7a679
John Edwin Guerrero Ayala <je.guerreroa1@uniandes.edu.co>
2023-02-18 16:43:38 -0500

    fix: Corrige instancia de esquema de db

--- vistas/vista_entrenadores.py
+++ vistas/vista_entrenadores.py
@@ -4,22 +4,11 @@ from flask_restful import Resource
 
 from modelos import db, EntrenadorSchema, Entrenador
 
-entrenador_schema = EntrenadorSchema
+entrenador_schema = EntrenadorSchema()
+
 
 class VistaEntrenadores(Resource):
     @jwt_required()
     def get(self):
         entrenadores = Entrenador.query.all()
         return [entrenador_schema.dump(entrenador) for entrenador in entrenadores]
-
-    @jwt_required()
-    def post(self):
-        nuevo_ejercicio = Ejercicio( \
-            nombre=request.json["nombre"], \
-            descripcion=request.json["descripcion"], \
-            video=request.json["video"], \
-            calorias=float(request.json["calorias"]),
-        )
-        db.session.add(nuevo_ejercicio)
-        db.session.commit()
-        return entrenador_schema.dump(nuevo_ejercicio)
